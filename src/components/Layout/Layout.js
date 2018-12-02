1e22aad1911903234a7aa1014ab762243433986d
mkzarra <mkzarra@gmail.com>
2018-12-02 17:44:31 -0500

    added logo and toggle side drawer on mobile.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import classes from './Layout.module.css'
+import React, { Component } from 'react';
 
-const layout = props => (
-  <>
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </>
-);
+import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-export default layout
+export default class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
+  
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  }
+
+
+  render() {
+    return (
+      <>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </>
+    );
+  }
+}
