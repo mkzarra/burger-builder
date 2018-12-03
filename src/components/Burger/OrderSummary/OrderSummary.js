a29a54665fa714840773f530fbe1c9d4771c54c9
mkzarra <mkzarra@gmail.com>
2018-12-03 09:06:19 -0500

    completed burger builder application

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,8 +4,9 @@ import Button from '../../UI/Button/Button';
 
 const orderSummary = props => {
   const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
-    return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>
-  })
+    return <li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}</li>
+  });
+
   return (
     <>
       <h3>Your Order</h3>
@@ -18,7 +19,7 @@ const orderSummary = props => {
       <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
       <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
     </>
-  )
-};
+  );
+}
 
 export default orderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react';
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = props => (
+export default class Modal extends Component {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show
+  }
+
+  render() {
+    return (
   <>
-    <Backdrop show={props.show} clicked={props.modalClose} />
+    <Backdrop show={this.props.show} clicked={this.props.modalClose} />
     <div
       className={classes.Modal}
       style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
+        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: this.props.show ? '1' : '0'
       }}>
-      {props.children}
+      {this.props.children}
     </div>
   </>
 );
-
-export default modal
+  }
+}
