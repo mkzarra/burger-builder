0f3e0ab71ece123ebefd42f3b626c103543667d5
mkzarra <mkzarra@gmail.com>
2019-01-08 11:44:51 -0500

    purchaseable UI handler updated.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,6 @@ import * as actionTypes from '../../store/actions';
 class BurgerBuilder extends Component {
 
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false
@@ -34,7 +33,7 @@ class BurgerBuilder extends Component {
     }).reduce((sum, el) => {
       return sum + el
       }, 0)
-    this.setState({purchasable: sum > 0})
+    return sum > 0;
   }
 
   purchaseHandler = () => {
@@ -78,7 +77,7 @@ class BurgerBuilder extends Component {
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
             price={this.props.price}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
           />
         </>
