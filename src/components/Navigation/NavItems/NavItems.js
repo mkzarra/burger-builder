8d67d9857fb72f9f7e4852b0684c5c0c7d21efae
mkzarra <mkzarra@gmail.com>
2019-01-23 10:12:10 -0500

    created logout container. Redirects user to burger builder on Logout

--- src/components/Navigation/NavItems/NavItems.js
+++ src/components/Navigation/NavItems/NavItems.js
@@ -6,7 +6,7 @@ import NavItem from './NavItem/NavItem'
 const navItems = (props) => (
   <ul className={classes.NavItems}>
     <NavItem link="/" exact>Burger Builder</NavItem>
-    <NavItem link="/orders">Orders</NavItem>
+    {props.isAuthenticated ? <NavItem link="/orders">Orders</NavItem> : null}
     {!props.isAuthenticated ? <NavItem link="/auth">Authenticate</NavItem> : <NavItem link="/logout">Logout</NavItem>}
   </ul>
 );
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -25,7 +25,7 @@ const authFail = (state, action) => {
   return updateObject(state, { error: action.error, loading: false });
 }
 
-const autLogout = (state, action) => {
+const authLogout = (state, action) => {
   return updateObject(state, { token: null, userId: null });
 }
 
@@ -34,7 +34,7 @@ const reducer = (state = initialState, action) => {
     case actionTypes.AUTH_START: return authStart(state, action);
     case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
     case actionTypes.AUTH_FAIL: return authFail(state, action);
-    case actionTypes.AUTH_LOGOUT: return autLogout(state, action);
+    case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
     default: return state;
   }
 }
